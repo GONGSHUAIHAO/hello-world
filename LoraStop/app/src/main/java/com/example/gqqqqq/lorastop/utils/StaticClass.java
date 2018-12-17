ee008e1938a783a01ce759358c4901b844f74c99
GQQQQQQ <369787331@qq.com>
2018-12-17 15:08:25 +0800

    remove lorastop

--- LoraStop/app/src/main/java/com/example/gqqqqq/lorastop/utils/StaticClass.java
+++
@@ -1,5 +0,0 @@
-package com.example.gqqqqq.lorastop.utils;
-
-public class StaticClass {
-
-}
--- LoraStop/app/src/test/java/com/example/gqqqqq/lorastop/entity/MyLocationListenerTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.gqqqqq.lorastop.entity;
-
-import static org.junit.Assert.*;
-
-public class MyLocationListenerTest {
-
-}
